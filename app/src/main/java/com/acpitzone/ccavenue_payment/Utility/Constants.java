be05e48583cea5090d20562a99b00196f265b242
Nafas Saadat <nafas.saadat2020@gmail.com>
2022-04-09 21:24:51 +0430

    Update Constants.java

--- app/src/main/java/com/acpitzone/ccavenue_payment/Utility/Constants.java
+++ app/src/main/java/com/acpitzone/ccavenue_payment/Utility/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
 	public static final String access_code = "4YRUXLSRO20O8NIH"; // add your access_code
 	public static final String merchantId= "2"; // add your merchant_id
 	public static final String currency="INR";
-	public static final String redirectUrl="http://122.182.6.216/merchant/ccavResponseHandler.jsp"; //this is test url you can modify with your on url
+	public static final String redirectUrl="http://122.182.6.216/merchant/ccavResponseHandler.jsp"; //this is test url you can modify with your on url, you can use php or jsp, 
 	public static final String cancelUrl="http://122.182.6.216/merchant/ccavResponseHandler.jsp"; //this is test url you can modify with your on url
 	public static final String rsaKeyUrl="https://secure.ccavenue.com/transaction/jsp/GetRSA.jsp"; //this is test url you can modify with your on url
 
